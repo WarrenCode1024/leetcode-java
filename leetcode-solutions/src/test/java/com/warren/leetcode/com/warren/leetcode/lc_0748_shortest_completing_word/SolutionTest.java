284d6e3325a69c09e9792290089b5d23aba42307
Navin Wei <warrenwei1024@gmail.com>
2025-08-14 01:26:32 +0800

    leetcode 0748

---
+++ leetcode-solutions/src/test/java/com/warren/leetcode/com/warren/leetcode/lc_0748_shortest_completing_word/SolutionTest.java
@@ -0,0 +1,20 @@
+package com.warren.leetcode.com.warren.leetcode.lc_0748_shortest_completing_word;
+
+import com.warren.leetcode.lc_0748_shortest_completing_word.Solution;
+import org.junit.Test;
+
+/**
+ * @author
+ * @desc
+ * @date 2025/8/14 0:28
+ */
+
+public class SolutionTest {
+    @Test
+    public void testShortestCompletingWord(){
+        Solution solution = new Solution();
+        String licensePlate = "1s3 456";
+        String[] words = {"looks","pest","stew","show"};
+        System.out.println(solution.shortestCompletingWord(licensePlate, words));
+    }
+}
