7d942702f50713c1c94dd24e60d5c4d454b275b8
WarrenCode1024 <warrenwei1024@gmail.com>
2025-08-17 14:47:55 +0800

    leetcode 0239

---
+++ leetcode-solutions/src/main/java/com/warren/leetcode/lc_0239_sliding_window_maximum/Solution.java
@@ -0,0 +1,48 @@
+package com.warren.leetcode.lc_0239_sliding_window_maximum;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author
+ * @desc 239. 滑动窗口最大值
+ * @date 2025/8/16 17:02
+ */
+
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null || nums.length == 0 || k == 0) return new int[0];
+
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();  // 存下标
+
+        for (int i = 0; i < n; i++) {
+            // 移除不在窗口的元素下标
+            while(!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            // 保持队列单调递减，移除比当前元素小的
+            while(!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            // 记录窗口最大值（从第 k-1 开始）
+            if (i >= k - 1) {
+                res[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
+        int[] res = solution.maxSlidingWindow(nums, 3);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
